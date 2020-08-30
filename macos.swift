2323ed3bd756be1768ec093a975a954c32c68ce4
harleyoleary <70482723+harleyoleary@users.noreply.github.com>
2020-08-30 18:54:23 +0100

    Mac OS Swift Source

---
+++ macos.swift
@@ -0,0 +1,23 @@
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'macos.swift'
+            if let scene = SKScene(fileNamed: "NSController") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+            
+            preview.scale.nscontroller = macos 10.0.0.0++ __ ==
